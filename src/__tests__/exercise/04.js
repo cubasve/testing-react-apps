a6236473afd68e5e924a56f2e486956ac206f178
cubasve <eva.cubasvasquez@gmail.com>
2022-05-29 00:56:57 -0400

    Test login form - jest mock fn, generate test data, allow overrides, use test data bot

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,28 +5,45 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
+
+const buildLoginForm1 = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
+  },
+})
+
+function buildLoginForm2(overrides) {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides,
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  let submittedData
-  const handleSubmit = data => (submittedData = data)
+  const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-
-  const usernameField = 'chucknorris'
-  const passwordField = 'i need no password'
+  const {username: usernameField, password: passwordField} = buildLoginForm2({
+    username: 'chucknorris',
+  })
 
   // screen.getByRole('textbox', {name: /username/i})
   const username = screen.getByLabelText(/username/i)
   const password = screen.getByLabelText(/password/i)
   const button = screen.getByRole('button', {name: /submit/i})
 
-  userEvent.type(username, usernameField)
+  userEvent.type(screen.getByLabelText(/username/i), usernameField)
   userEvent.type(password, passwordField)
   await userEvent.click(button)
 
-  expect(submittedData).toEqual({
+  expect(handleSubmit).toHaveBeenCalledWith({
     username: usernameField,
     password: passwordField,
   })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
